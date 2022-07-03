e8808cbf9f7c390497260ddfc47d9b754b989644
7474 <koudenpa@hatena.ne.jp>
2022-07-03 15:51:08 +0900

    matanai

--- nodejs/src/app.ts
+++ nodejs/src/app.ts
@@ -1141,7 +1141,59 @@ app.post(
       if (!isValidPostIsuConditionRequest(request) || request.length === 0) {
         return res.status(400).type("text").send("bad request body");
       }
+      postCondition(jiaIsuUUID, request);
+      // await db.beginTransaction();
 
+      // const [[{ cnt }]] = await db.query<(RowDataPacket & { cnt: number })[]>(
+      //   "SELECT COUNT(*) AS `cnt` FROM `isu` WHERE `jia_isu_uuid` = ?",
+      //   [jiaIsuUUID]
+      // );
+      // if (cnt === 0) {
+      //   await db.rollback();
+      //   return res.status(404).type("text").send("not found: isu");
+      // }
+
+      // for (const cond of request) {
+      //   if (!isValidConditionFormat(cond.condition)) {
+      //     await db.rollback();
+      //     return res.status(400).type("text").send("bad request body");
+      //   }
+      // }
+
+      // await db.query(
+      //   "INSERT INTO `isu_condition`" +
+      //     "	(`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`)" +
+      //     "	VALUES " +
+      //     request.map((_) => "(?, ?, ?, ?, ?)").join(","),
+      //   request
+      //     .map((cond) => [
+      //       jiaIsuUUID,
+      //       new Date(cond.timestamp * 1000),
+      //       cond.is_sitting,
+      //       cond.condition,
+      //       cond.message,
+      //     ])
+      //     .flat()
+      // );
+
+      // await updateLatestCondition(db, jiaIsuUUID);
+
+      // await db.commit();
+
+      return res.status(202).send();
+    } catch (err) {
+      console.error(`db error: ${err}`);
+      await db.rollback();
+      return res.status(500).send();
+    } finally {
+      db.release();
+    }
+  }
+);
+
+async function postCondition(jiaIsuUUID: string, conds: any) {
+  const db = await pool.getConnection();
+    try {
       await db.beginTransaction();
 
       const [[{ cnt }]] = await db.query<(RowDataPacket & { cnt: number })[]>(
@@ -1150,13 +1202,12 @@ app.post(
       );
       if (cnt === 0) {
         await db.rollback();
-        return res.status(404).type("text").send("not found: isu");
+        throw "not found: isu";
       }
 
-      for (const cond of request) {
+      for (const cond of conds) {
         if (!isValidConditionFormat(cond.condition)) {
-          await db.rollback();
-          return res.status(400).type("text").send("bad request body");
+          throw "bad request body";
         }
       }
 
@@ -1164,9 +1215,8 @@ app.post(
         "INSERT INTO `isu_condition`" +
           "	(`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`)" +
           "	VALUES " +
-          request.map((_) => "(?, ?, ?, ?, ?)").join(","),
-        request
-          .map((cond) => [
+          conds.map((_) => "(?, ?, ?, ?, ?)").join(","),
+          conds.map((cond) => [
             jiaIsuUUID,
             new Date(cond.timestamp * 1000),
             cond.is_sitting,
@@ -1179,17 +1229,13 @@ app.post(
       await updateLatestCondition(db, jiaIsuUUID);
 
       await db.commit();
-
-      return res.status(202).send();
     } catch (err) {
       console.error(`db error: ${err}`);
       await db.rollback();
-      return res.status(500).send();
     } finally {
       db.release();
     }
-  }
-);
+}
 
 async function updateLatestCondition(db: any, jia_isu_uuid: string) {
   await db.query(

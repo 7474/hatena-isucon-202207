423e7d08031028c815edd281b564fce291dbfd91
7474 <koudenpa@hatena.ne.jp>
2022-07-03 14:49:28 +0900

    init last onitoin

--- sql/2_Alter.sql
+++ sql/2_Alter.sql
@@ -2,3 +2,8 @@
 ALTER TABLE isu
   ADD `last_condition_id` bigint DEFAULT NULL
   ;
+
+-- init
+UPDATE `isu` SET last_condition_id = (
+  SELECT id FROM isu_condition WHERE `jia_isu_uuid` = isu.jia_isu_uuid ORDER BY `timestamp` DESC LIMIT 1
+);
